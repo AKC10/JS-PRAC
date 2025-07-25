b1435e88d5e003b1ee756733b138ab3a5cdbffc1
Ash <koirala.ashrut18@gmail.com>
2025-07-25 17:54:48 -0400

    Update and rename DogYears to DogYears.js

---
+++ DogYears.js
@@ -0,0 +1,18 @@
+//Age of person
+const myAge = 21;
+let earlyYears = 2;
+//early years in dog years
+earlyYears *= 10.5;
+//subtracted 2 because early years is already 2
+let laterYears = myAge -2;
+laterYears *= 4;
+//adding early and later years to get dog years 
+let myAgeInDogYears = earlyYears + laterYears;
+//assigned name as string
+NAME = 'Monkey D Luffy'.toLowerCase();
+//print the statement "My name is ... I am __ years old in dog years"
+console.log(`My name is ${NAME}. I am ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
